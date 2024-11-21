0ed48a196c1423733c2883b11af26e0263f7f471
Iqbql Hossain Chapal <chapal2004@gmail.com>
2024-11-21 18:12:51 +0900

    banner slider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 
 import "./globals.css";
+import ThemeProvider from "@/context/ThemeProvider";
 
 
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {children}
+       <ThemeProvider>
+       {children}
+       </ThemeProvider>
       </body>
     </html>
   );
---
+++ src/components/BannerSlider/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BannerSlider = () => {
+  return (
+    <>
+    <h1> Banner slider</h1>
+    </>
+  )
+}
