1d24f48402815602ace2f6454e9b4c30675231cf
Iqbql Hossain Chapal <chapal2004@gmail.com>
2024-11-21 17:49:35 +0900

    second commit

---
+++ src/context/ThemeProvider.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useState, useEffect, } from "react"
+import { createContext, MyContext } from "./ThemeContext";
+
+
+
+const ThemeProvider = ({children}) => {
+
+    const values = {
+
+    }
+
+
+    return (
+        <MyContext.Provider value={values}>
+
+            {children}
+            </MyContext.Provider>
+    )
+} 
+export default ThemeProvider;
