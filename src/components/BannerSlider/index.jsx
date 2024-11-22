24062822f6926e13473668d311d77d6997abadac
Iqbql Hossain Chapal <chapal2004@gmail.com>
2024-11-22 21:01:56 +0900

    banner left right

--- src/components/BannerSlider/index.jsx
+++ src/components/BannerSlider/index.jsx
@@ -31,27 +31,26 @@ export const BannerSlider = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="banner-slider w-[100%]" 
       >
+        
         <SwiperSlide>
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide1.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-red-600 text-2xl lg:text-8xl ">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-7xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
         </SwiperSlide>
-
        
-
         <SwiperSlide>
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide2.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-red-600 text-2xl lg:text-8xl">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 ml-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-7xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
@@ -61,50 +60,49 @@ export const BannerSlider = () => {
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide3.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-white">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-6xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
         </SwiperSlide>
 
-        <SwiperSlide>
+         <SwiperSlide>
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide4.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-white">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 ml-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-6xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
         </SwiperSlide>
 
-        <SwiperSlide>
+         <SwiperSlide>
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide-5.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-white">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-6xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
         </SwiperSlide>
 
-        <SwiperSlide>
+         <SwiperSlide>
         <div className="item w-[100%] relative flex items-center pl-[100px]"
           style={{background: `url(/slide6.png)`}}>
           <div className="container">
-          <div className="info w-[50%] d-flex flex-col gap-4 mr-auto duration-1000">
-              <h2 className="text-white">Body and Skin Fit</h2>
-              <h3><span className="text-white/80">Fress Every Day </span><span className="text-yellow-600">Like Nature</span></h3>
+          <div className="info w-[50%] d-flex flex-col gap-4 ml-auto mb-56 duration-6000">
+              <h2 className="text-red-600 text-4xl lg:text-6xl mb-8">Body and Skin Fit</h2>
+              <h3><span className="text-white/80 text-2xl lg:text-4xl ">Fress Every Day </span><span className="text-yellow-600 text-2xl lg:text-4xl">Like Nature</span></h3>
           </div>
           </div>
         </div>
-        </SwiperSlide>
-        
+        </SwiperSlide>     
       </Swiper>
     </>
   );
